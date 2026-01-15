4abac012b5903f4ac5252ecabbb83f591aad9efb
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-15 13:38:02 +0530

    (DB-Connection): Create the mongodb connection and add it to the App.js file to console the message of DB connection.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const connectionDB = require('./DBConnection/dbconnection');
+connectionDB();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
+
 
 const PORT = 5000;
 app.listen(PORT, () => {
