7cf85f2665fe1335de6a4ba9dd3a4ea07274dc87
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-16 11:12:15 +0530

    (DB-Connection): Create the new folder call Login and create the file call login.js and add the user login part.

---
+++ Login/login.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const collaction = require('../DBConnection/dbconnectionschema');
+
+router.post('/login', (req, res)=>{
+    const {username, password} = req.body;
+
+    collaction.findOne({username:username})
+    .then(user=>{
+        if(user){
+            bcrypt.compare(password,user.password, (error, response)=>{
+                if(response){
+                    res.json('Successful login')
+                }else{
+                    res.json('login error')
+                }
+            })
+        }else{
+            res.json('no data found')
+        }
+    })
+});
+
+module.exports = router;
