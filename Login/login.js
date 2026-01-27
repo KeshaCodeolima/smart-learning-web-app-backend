29c032e866f1ecb20fd12ad770da6dbc1ad8d3cf
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-01-27 13:18:24 +0530

    (DB-Connection): change some login code get the user details.

--- Login/login.js
+++ Login/login.js
@@ -3,23 +3,30 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const collaction = require('../DBConnection/dbconnectionschema');
 
-router.post('/login', (req, res)=>{
-    const {username, password} = req.body;
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
 
-    collaction.findOne({username:username})
-    .then(user=>{
-        if(user){
-            bcrypt.compare(password,user.password, (error, response)=>{
-                if(response){
-                    res.json('Successful login')
-                }else{
-                    res.json('login error')
-                }
-            })
-        }else{
-            res.json('no data found')
-        }
-    })
+    collaction.findOne({ username: username })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password, (error, response) => {
+                    if (response) {
+                        res.json({
+                            message: 'Successful Login',
+                            user: {
+                                username: user.username,
+                                email: user.email,
+                                name: user.name
+                            }
+                        });
+                    } else {
+                        res.json('login error')
+                    }
+                })
+            } else {
+                res.json('no data found')
+            }
+        })
 });
 
 module.exports = router;
