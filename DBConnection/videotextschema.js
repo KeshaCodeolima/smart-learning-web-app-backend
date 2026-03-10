c5ee4689fcd886b6f464c9ddfee65fe19e91551b
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-03-10 12:41:25 +0530

    (Quiz-Note-Creation): Add the videotextschema.js file inside the DBConnection folder to write the video to text files save in mongodb schema.

---
+++ DBConnection/videotextschema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const transcriptSchema = new mongoose.Schema({
+    userId: String,
+    videoName: String,
+    transcript: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Transcript', transcriptSchema);
