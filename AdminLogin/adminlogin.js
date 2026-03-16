1d95e01516a7b02ca8414bceedb55f7106173cbf
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-03-16 16:18:35 +0530

    (Admin): Add the code lines to add admin login part.

---
+++ AdminLogin/adminlogin.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../DBConnection/adminschema");
+const bcrypt = require("bcrypt");
+
+router.post("/admin-login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.json("User not found");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.json("Invalid password");
+
+    res.json({
+      message: "Admin login successful",
+      admin: {
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server error");
+  }
+});
+
+module.exports = router;
