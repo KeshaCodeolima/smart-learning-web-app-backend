bea4da5c7145bd3aebf702d75585b5b437e52157
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-03-25 02:52:23 +0530

    (Feature-VideoText): Add the code lines to get the Database store language and pass to the frontend.

--- VideoText/videotextgetting.js
+++ VideoText/videotextgetting.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Transcript = require('../DBConnection/videotextschema')
+const Transcript = require('../DBConnection/videotextschema');
+const { Language } = require('@google/genai');
 
 router.post('/text', async (req, res) => {
     try {
         const { userId } = req.body;
         const transcriptDoc = await Transcript.findOne({ userId })
-        res.json({ text: transcriptDoc.transcript })
+        res.json({
+            text: transcriptDoc.transcript,
+            language: transcriptDoc.language
+        })
     } catch (error) {
         console.error("Error fetching transcript:", error);
         res.status(500).json({ error: "Server error" });
